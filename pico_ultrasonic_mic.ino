7f5db3c023380daa632c3f638a35f899c6d9d955
Chris Thompson <g0kla@arrl.net>
2025-05-20 17:53:29 -0400

    Small updates for debugging

--- pico_ultrasonic_mic.ino
+++ pico_ultrasonic_mic.ino
@@ -1,6 +1,18 @@
 #include "hardware/adc.h"
+#include <math.h>
+#include <stdio.h>
 
 #define busyPin1 3
+#define nFFT 64
+#define DCremove 1
+#define kHz 1e3
+
+float fMin=0 ;
+float fMax=0 ;
+int nMean=0;
+float fftRe[nFFT] ;
+float fftIm[nFFT] ;
+int kMin,kMax ;
 
 #define cpuClkKhz 125000 // data sheet says 133000 maximum
 //#define ADCclkDiv1 96  // 500 kHz samplesrate
@@ -9,17 +21,25 @@
 #define ADCclkDiv1 192  // 250 kHz samplesrate
 
 /* global variables */
+float sampleRate=0;
 char stxt[128] ;
-#define nSamples 1024
-#define nPreamble 128
+//#define nSamples 1024
+#define nPreamble 16
 int preambleBuffer[nPreamble] ;
-int16_t sampleBuffer[nSamples] ;
+int sampleBuffer[nFFT] ;
 
 /* First we have any needed functions.  Setup and loop are at the end */
+float sqr(float x){
+  return x*x ;
+}
+
+float sampleBufferVolt(int k){
+  float vv=(sampleBuffer[k]-2048)/4096.0*3.3 ;
+  return vv ;
+}
 
 void adcSetClockRate(int ADCclkDiv){
   int adcEffectiveDivider ;
-  int sampleRate ;
   if(ADCclkDiv<=96){
     adc_set_clkdiv (0) ; 
     adcEffectiveDivider=96 ; 
@@ -52,9 +72,10 @@ void adcInit(void) {
   adc_init(); // hardware API
 
   // Make sure GPIO is high-impedance, no pullups etc
-  adc_gpio_init(27);
+  adc_gpio_init(26); //27
+  // Select ADC input 0 (GPIO26)
   // Select ADC input 1 (GPIO27)
-  adc_select_input(1);
+  adc_select_input(0);
 
   adcSetClockRate(ADCclkDiv1) ;
   adc_fifo_setup(true, false, 0, false, false);
@@ -110,7 +131,7 @@ void old_sendBuffer(){
 
   uart_putc1('S') ;
   uart_putc1('p') ;decout6(nPreamble) ;
-  uart_putc1('n') ;decout6(nSamples) ;
+  uart_putc1('n') ;decout6(nFFT) ;
   
   uart_putc1('d') ; decout6( ADCclkDiv1 ) ;
   uart_putc1('c') ; decout6( cpuClkKhz ) ;
@@ -121,7 +142,7 @@ void old_sendBuffer(){
     hexOut3(v) ;
     }
 
-  for(int k=0 ; k<nSamples ; k++){
+  for(int k=0 ; k<nFFT ; k++){
     int v=(sampleBuffer[k]) & 0xFFF ;
     chkSum+=v ;
     hexOut3(v) ;
@@ -135,13 +156,22 @@ void old_sendBuffer(){
 
 void send_samples() {
   Serial.println("DATA:");
-  for(int k=0 ; k<nSamples ; k++){
+  for(int k=0 ; k<nFFT ; k++){
     Serial.println(sampleBuffer[k]);
+    }
+}
+
+void send_spectrum() {
+  Serial.println("FFT:");
+  for(int k=0 ; k<=nFFT/2 ; k++){
+      Serial.println(sqrt(sqr((float)fftRe[k])+sqr((float)fftIm[k])));
     }
 }
 
 float windowFactor=1.0 ;
 
+/* Fast Fourier Transform from Elektor Magazine Pico SA Article Sept/Oct 2023
+   by Prof. Martin Ossmann*/
 void fft1(int flag , double scale ) {
     int j,k,l,m,m1,n1,r,r1,r2 ;
     double re,im,a,b,ru,rv,iu,iv,rs ;
@@ -192,6 +222,7 @@ void fft1(int flag , double scale ) {
       }
     }
 
+/* Blackman window function */
 double windowFun(int k){
   double alpha=0.16 ;
   double a0=(1-alpha)/2 ;
@@ -201,22 +232,18 @@ double windowFun(int k){
   return w ;
   }
   
- float getWindowFactorOLD(){
-    float mean1=0 ;
-    for (int k=0; k<nFFT; k++) {
-      mean1 += sqr(windowFun(k))  ;
-      }
-    mean1=sqrt(mean1/nFFT) ;  
-    return mean1 ; 
-    }
 
+/* To compensate for the signal reduction by applying the window we divide the
+   spectrum by the window factor.  This is the arithmetic mean of the window function values */
 float getWindowFactor(){
+  char ptxt[128];
     float mean1=0 ;
     for (int k=0; k<nFFT; k++) {
       mean1 += fabs(windowFun(k))  ;
       }
     mean1=mean1/nFFT ;  
-    printf("windowFactor=%8.3f\n",mean1) ;
+    // sprintf(ptxt, "windowFactor=%8.3f\n",mean1) ;
+    // Serial.println(ptxt);
     return mean1 ; 
     }
 
@@ -239,6 +266,46 @@ void windowedFFT(int flag , double scale ){
   fft1(flag ,scale/windowFactor) ;
   }
 
+void spectrum_analysis() {
+  for (int k=0; k<nFFT ; k++) {
+    //fftRe[k]=cos(2*PI*k*64.5/nFFT)+0.01*random(1.0) ;
+    // float frq=30e3 ;
+    // fftRe[k]=cos(2*M_PI*k*frq/sampleRate) +0.001*( rand()%10-5) ;
+    //frq=5.0 ;
+    // fftRe[k]=cos(2*M_PI*k*frq/nFFT) +0.001*( rand()%10-5) ;
+    fftRe[k]=sampleBufferVolt(k) ;
+    // printf("k=%3d sampleBufferVolt=%8.3f\n",k,sampleBufferVolt(k)) ;
+    fftIm[k]=0 ;
+  }
+
+  if(DCremove){
+    float DCsum=0.0 ;
+    for (int k=0; k<nFFT ; k++) { DCsum += fftRe[k] ; }
+    DCsum=DCsum/nFFT ;
+    for (int k=0; k<nFFT ; k++) { fftRe[k] -= DCsum ; }
+  }
+
+  windowedFFT(1,2.0/nFFT);
+
+  float maxFFT=0.0 ;
+  float maxFrq=0.0 ;
+  int maxK=0 ;
+  for(int k=kMin ; k<kMax ; k++){
+     // printf("k=%4d FFTre=%8.3f   FFTim=%8.3f\n",k,fftRe[k],fftIm[k]) ;
+    float v=sqrt(sqr((float)fftRe[k])+sqr((float)fftIm[k])) ;
+    if(v>maxFFT){
+      maxFFT=v ;
+      maxFrq=((float) k) *sampleRate/((float)nFFT) ;
+      maxK=k ;
+      sprintf(stxt,"set maxK=%d maxFrq=%8.3f kHz   maxFFT=%8.3f\n",maxK,maxFrq/kHz,maxFFT) ;  
+      Serial.println(stxt);
+      sprintf(stxt,"fSample= %10.1f kHz",sampleRate/1e3) ;
+      Serial.println(stxt);
+      
+    }
+  }
+
+}
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -250,6 +317,21 @@ void setup() {
 
   gpio_init(busyPin1);
   gpio_set_dir(busyPin1, GPIO_OUT);
+
+  for (int k=0; k<nFFT ; k++) {
+    fftRe[k]=0 ;
+    fftIm[k]=0 ;
+  }
+
+  windowFactor=getWindowFactor() ;
+ // nMean=0;  
+  
+  fMin=0 ;
+  fMax=sampleRate/2 ;
+
+  kMin=(int)(fMin/sampleRate*nFFT) ;
+  kMax=(int)(fMax/sampleRate*nFFT) ;
+  if(kMax>nFFT/2){ kMax=nFFT/2 ; }
 
   /* Setup the ADC */
   adcInit();
@@ -267,36 +349,29 @@ void setup() {
 }
 
 void loop() { 
-    /// waitforDMA() ;
-    adc_run(false);
-    adc_fifo_drain();
-    adc_run(true);
-    // for(int k=0 ; k<100 ; k++){
-    //   adc_fifo_get_blocking() ;
-    // }
-    // for(int k=0 ; k<nPreamble ; k++){
-    //   preambleBuffer[k]=adc_fifo_get_blocking() ;
-    // }
-    gpio_put(busyPin1,1) ;  
-    for(int k=0 ; k<nSamples ; k++){
-      sampleBuffer[k]=adc_fifo_get_blocking() ; ;
-     //  sampleBuffer[k]=k ;
-    }
-    gpio_put(busyPin1,0) ; 
-    adc_run(false);
-    //sleep_ms(1000) ;
-    send_samples();
-    send_fft_result();
-//    sendBuffer() ;  
-   
-  // /* Read the ADC */
-  // uint32_t result = adc_read();
-  // const float conversion_factor = 3.3f / (1 << 12);
-    
-  // /* Calculate the data */
+  /// waitforDMA() ;
+  adc_run(false);
+  adc_fifo_drain();
+  adc_run(true);
 
-  // /* Send the data */
-  // Serial.println("Data...");
-  // sprintf(stxt, "0x%03x -> %f V", result, result * conversion_factor);
-  // Serial.println(stxt);
+  /* TODO Not sure why we do this ..*/
+  for(int k=0 ; k<100 ; k++){
+    adc_fifo_get_blocking() ;
+  }
+  for(int k=0 ; k<nPreamble ; k++){
+    preambleBuffer[k]=adc_fifo_get_blocking() ;
+  }
+
+
+  gpio_put(busyPin1, 1);
+  for (int k = 0; k < nFFT; k++) {
+    sampleBuffer[k] = adc_fifo_get_blocking();
+    //  sampleBuffer[k]=k ;
+  }
+  gpio_put(busyPin1, 0);
+  adc_run(false);
+  send_samples();
+  //spectrum_analysis();
+  //send_spectrum();
+  sleep_ms(10000) ;
 }
